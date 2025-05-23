40c33145bd1ffe7d90e97a29d70217e80e3787b1
Thomaaseth <thomas.demathieu@gmail.com>
2025-05-23 11:47:37 +0200

    update

--- src/api/handlerReset.ts
+++ src/api/handlerReset.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from "express";
 import { config } from "../config.js";
 
-export function handlerReset(req: Request, res: Response) {
+export async function handlerReset(req: Request, res: Response) {
     config.fileserverHits = 0
     res.send("Hits reset to 0")
     res.end()
--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,19 @@
+process.loadEnvFile();
+
+function envOrThrow(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Environment variable ${key} is required`);
+  }
+  return value;
+}
 
 type APIConfig = {
-    fileserverHits: number;
+  fileserverHits: number;
+  dbURL: string;
 }
 
 export const config: APIConfig = {
-    fileserverHits: 0
+  fileserverHits: 0,
+  dbURL: envOrThrow("DB_URL"),
 }
